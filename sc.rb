9f5ffcc3f6c066f6d64f8acd3ee11c3b80845501
alex@recordsonribs.com <alex@recordsonribs.com>
2010-05-23 15:36:46 +0100

    Basic skeleton of the program for uploads.

--- sc.rb
+++ sc.rb
@@ -5,6 +5,25 @@ require 'rexml/document'
 
 include REXML
 
+class SCFile  
+  @@files_count = 0
+  
+  attr_writer :filename
+  attr_writer :type
+  attr_writer :title
+  attr_writer :bpm
+  
+  def initialize(filename, type)
+      @filename = filename
+      @type = type
+      @@files_count += 1
+  end
+  
+  def file_count
+      return "#{@@files_count}"
+  end
+end
+
 program :name, 'Soundcloud CLI'
 program :version, '0.1'
 program :description, 'Command-line interface to SoundCloud.'
@@ -23,9 +42,54 @@ end
 
 command :upload do |c|
   c.syntax = 'sc upload [tracks or track directory]'
-  c.description = 'Uploads Flac specified on command line to Soundcloud.'
+  c.description = 'Uploads file(s) specified on command line to Soundcloud.'
   
+  # Array of SoundCloud Files
+  upfiles = Array.new
+
   c.when_called do |args, options|
-   # Do something with the files here
-  end
+    args.each do | arg |
+      if File.directory?(arg)
+         Dir.foreach(arg) do |f| 
+           if File.extname(f) == '.flac'
+             file = SCFile.new(f,'flac')
+             file.title = 'Test'
+             file.bpm = '0'
+             upfiles.push(file)   
+           end
+         end
+      else
+        if !File.exist?(arg)
+          puts "File #{arg} does not exist, bailing."
+          exit
+        end
+        file = SCFile.new(arg,'flac')
+        file.title = 'Test'
+        file.bpm = '0'
+        upfiles.push(file)
+      end # File type
+    end  # Argument parsing.
+    
+    if agree("Are these files going to be part of a set? ")
+      # We could make a few guesses here from reading the files, ie album tags
+      set_title = ask("Title: ")
+      set_desc = ask("Description:")
+      set_genre = ask("Genre: ")
+      set_label = ask("Record Label: ")
+      set_date = ask_for_date("Release Date (yy-mm-dd): ")
+      set_label = ask("EAN/UPC: ")
+      set_buy = ask("Buy this set link: ")
+      set_tags = ask_for_array("Tags (seperated by space): ")
+      
+      # Sort out license
+      choose do |menu|
+        menu.prompt = "Please choose the license for this set?  "
+        
+        menu.choices(:all_rights_reserved, :cc_by) do 
+          # put the stuff in a variable
+        end
+      end
+    end # End setup set
+    
+  end # Action
 end
