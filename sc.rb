038efb6698219619dfe395d143f8ea2933b8ac32
alex@recordsonribs.com <alex@recordsonribs.com>
2010-07-16 23:15:33 +0100

    - Stripped the files back to integrate the SoundCloud provided additions - Added SoundCloud provided files

--- sc.rb
+++ sc.rb
@@ -4,40 +4,14 @@ gem 'flacinfo-rb'
 
 require 'commander/import' # Collection of CLI in a API - http://visionmedia.github.com/commander/
 require 'flacinfo' # FlacInfo library for reading tags from FLAC files
-require 'rexml/document'
 
-include REXML
-
-class SCFile  
-  @@files_count = 0
-  
-  attr_writer :filename, :type, :title,:bpm
-  attr_reader :filename, :type, :title,:bpm, :files_count
-  
-  def initialize()
-      @@files_count += 1
-  end
-  
-end
+require 'setup'
 
 program :name, 'Soundcloud CLI'
 program :version, '0.1'
 program :description, 'Command-line interface to SoundCloud.'
 program :help, 'Author', 'Alex Andrews (alex@recordonribs.com)'
 program :int_message, 'Interupted, bailing.'
-
-# Load username and password if we have them from sc.xml
-if File.exist?('sc.xml')
-  conf = Document.new File.new('sc.xml')
-  uname = XPath.first( conf, "//username" ).text
-  pword = XPath.first( conf, "//password" ).text
-  license = XPath.first( conf, "//license" ).text
-else
-  uname = ask("SoundCloud Username: ")
-  pword = password
-  
-  # Output to file
-end
 
 command :upload do |c|
   c.syntax = 'sc upload [tracks or track directory]'
@@ -70,14 +44,4 @@ command :upload do |c|
       end # File type
     end  # Argument parsing.
   end # Action
-end
-
-command :setup do |c|
-  c.syntax = 'sc setup'
-  c.description = 'Sets default settings for SoundCloud upload and writes them to configuration file.'
-end
-
-command :download do |c|
-  c.syntax = 'sc download [title]'
-  c.description = 'Attempts to download the file from SoundCloud with the title specified.'
 end
